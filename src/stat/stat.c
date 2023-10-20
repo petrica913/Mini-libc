3738cf9bc0048e2f142b8ada226dbb7782553144
gheorghe.petrica <gheorghe.petrica@stud.acs.upb.ro>
2023-10-20 17:31:28 +0300

    stat functions

--- src/stat/stat.c
+++ src/stat/stat.c
@@ -7,6 +7,13 @@
 
 int stat(const char *restrict path, struct stat *restrict buf)
 {
-	/* TODO: Implement stat(). */
+	int ret = syscall(__NR_stat, path, buf);
+
+	if (ret < 0) {
+		errno = -ret;
+		return -1;
+	}
+
+	return ret;
 	return -1;
 }
