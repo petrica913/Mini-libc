e4529b058cddd5120693764ebf693d39e59ce2e7
gheorghe.petrica <gheorghe.petrica@stud.acs.upb.ro>
2023-10-22 12:48:02 +0300

    realloc implementation

--- src/mm/mmap.c
+++ src/mm/mmap.c
@@ -33,7 +33,6 @@ void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
 
 void *mremap(void *old_address, size_t old_size, size_t new_size, int flags)
 {
-	/* TODO: Implement mremap(). */
 	long result = syscall(__NR_mremap, old_address, old_size, new_size, flags);
 	
     if (result < 0) {
@@ -46,7 +45,6 @@ void *mremap(void *old_address, size_t old_size, size_t new_size, int flags)
 
 int munmap(void *addr, size_t length)
 {
-	/* TODO: Implement munmap(). */
 	long result = syscall(__NR_munmap, addr, length);
 	
     if (result < 0) {
