a818300e543ce5fead1846488d879e86c68b8d56
gheorghe.petrica <gheorghe.petrica@stud.acs.upb.ro>
2023-11-01 08:32:40 +0200

    update nanosleep

--- src/process/nanosleep.c
+++ src/process/nanosleep.c
@@ -3,5 +3,8 @@
 
 int nanosleep(const struct timespec *req, struct timespec *rem)
 {
-    return syscall(__NR_nanosleep, req, rem);
+    int result = syscall(__NR_nanosleep, req, rem);
+    if (result < 0)
+        return -1;
+    return result;
 }
